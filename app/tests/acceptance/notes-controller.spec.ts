4a91b0b3a674402abac248402c953aabeeb570f8
Miguel Matey Sanz <al341802@uji.es>
2023-07-06 14:34:11 +0200

    test(hu02): add acceptance tests

--- app/tests/acceptance/notes-controller.spec.ts
+++ app/tests/acceptance/notes-controller.spec.ts
@@ -16,6 +16,8 @@ describe("Notes controller acceptance tests", () => {
   const emptyTitle = "";
   const title1 = "Test title 1";
   const description1 = "Test description 1";
+  const title2 = "Test title 2";
+  const description2 = "Test description 2";
 
   beforeEach(() => {
     database = new CouchBase(DB_NAME);
@@ -44,6 +46,36 @@ describe("Notes controller acceptance tests", () => {
     // When: se intenta crear una nota sin título
     expect(() => notesController.createNote(emptyTitle, description1))
       .toThrow(new EmptyTitleError()); // Then: se lanza la excepción EmptyTitleError
+  });
+
+  it("H02_E01", async () => {
+    // Given: no hay ninguna nota
+
+    // When: se consultan las notas
+    const notes = await firstValueFrom(notesController.getNotes());
+
+    // Then: se obtiene una lista vacía
+    expect(notes.length).toBe(0);
+  });
+
+  it("H02_E02", async () => {
+    // Given: hay varias notas almacenadas
+    notesController.createNote(title1, description1);
+    notesController.createNote(title2, description2);
+
+    // When: se consultan las notas
+    const notes = await firstValueFrom(notesController.getNotes());
+
+    // Then: se obtiene una lista con las notas almacenadas
+    expect(notes.length).toBe(2);
+    expect(notes[0]).toEqual(jasmine.objectContaining({
+      title: title1,
+      description: description1
+    }));
+    expect(notes[1]).toEqual(jasmine.objectContaining({
+      title: title2,
+      description: description2
+    }));
   });
 
   afterEach(() => {
